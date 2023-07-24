9dfdd983e846bdc275c24fff7a187ca7a6b0b9c3
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2023-07-24 14:12:43 +0530

    Update textfilekv to v1.1.1

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.19
 require (
 	github.com/go-git/go-git/v5 v5.8.0
 	github.com/manifoldco/promptui v0.9.0
-	github.com/miteshbsjat/textfilekv v1.0.1
+	github.com/miteshbsjat/textfilekv v1.1.1
 	github.com/spf13/cobra v1.7.0
 	github.com/yang3yen/xxtea-go v1.0.3
 	gopkg.in/yaml.v3 v3.0.1
--- pkg/gitcloak/textfilekv.go
+++ pkg/gitcloak/textfilekv.go
@@ -1,6 +1,8 @@
 package gitcloak
 
 import (
+	"fmt"
+	"os"
 	"sync"
 
 	tkv "github.com/miteshbsjat/textfilekv"
@@ -9,9 +11,51 @@ import (
 var tkvInitialized sync.Once
 var tkvObj *tkv.KeyValueStore
 
+var kvStoreCache = struct {
+	mu    sync.RWMutex
+	cache map[string]*tkv.KeyValueStore
+}{
+	cache: make(map[string]*tkv.KeyValueStore),
+}
+
+// NewKVStore is a factory method that returns the appropriate KVStore instance based on the filename.
+func NewKVStore(file string) (*tkv.KeyValueStore, error) {
+	kvStoreCache.mu.RLock()
+	if kvs, ok := kvStoreCache.cache[file]; ok {
+		kvStoreCache.mu.RUnlock()
+		return kvs, nil
+	}
+	kvStoreCache.mu.RUnlock()
+
+	kvStoreCache.mu.Lock()
+	defer kvStoreCache.mu.Unlock()
+
+	filePath := GetGitCloakBase() + string(os.PathSeparator) + file + ".txt"
+	// Create a new KVStore only if it doesn't exist in the cache
+	switch file {
+	case "ggcmap":
+		kvs, err := tkv.NewKeyValueStore(filePath)
+		if err != nil {
+			return nil, err
+		}
+		kvStoreCache.cache[file] = kvs
+		return kvs, nil
+	case "filedata":
+		kvs, err := tkv.NewKeyValueStore(filePath)
+		if err != nil {
+			return nil, err
+		}
+		kvStoreCache.cache[file] = kvs
+		return kvs, nil
+	// Add more cases for different filenames if needed
+	default:
+		return nil, fmt.Errorf("unsupported filename: %s", file)
+	}
+}
+
 func initTextFileKV() {
 	tkvInitialized.Do(func() {
-		tkvObj = tkv.NewKeyValueStore(GetGitCloakBase() + "/ggcmap.txt")
+		tkvObj, _ = tkv.NewKeyValueStore(GetGitCloakBase() + "/ggcmap.txt")
 	})
 }
 
