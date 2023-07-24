9207dd87e3eea7cd84b9be2459d8f5af884d31f0
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2023-07-24 14:28:16 +0530

    Update tests for testfilekv factory

--- pkg/gitcloak/textfilekv_test.go
+++ pkg/gitcloak/textfilekv_test.go
@@ -17,5 +17,36 @@ func TestTextFileKV(t *testing.T) {
 	if ret != gcHash {
 		t.Errorf("%s != %s", ret, gcHash)
 	}
+}
 
+func TestTextFileKVFactor(t *testing.T) {
+	gckv, err := NewKVStore("ggcmap")
+	if err != nil {
+		t.Error(err)
+	}
+
+	gckv.Set("ab", "cd")
+	ret, _ := gckv.Get("ab")
+	if ret != "cd" {
+		t.Errorf("%s != cd", ret)
+	}
+
+	gHash := "abcde"
+	gcHash := "fghij"
+	gckv.Set(gHash, gcHash)
+	ret, _ = gckv.Get(gHash)
+	if ret != gcHash {
+		t.Errorf("%s != %s", ret, gcHash)
+	}
+
+	gckv1, err := NewKVStore("ggcmap")
+	if err != nil {
+		t.Error(err)
+	}
+	if gckv != gckv1 {
+		t.Errorf("%v != %v\n", gckv, gckv1)
+	}
+
+	gckv.Delete("ab")
+	gckv.Delete("abcde")
 }
