eea7a2a70ab2e13ec88acf4eae19dd98844a8893
Mitesh Singh Jat <mitesh.singh.jat@gmail.com>
2023-08-18 12:55:34 +0530

    Update gitcloak commit help message

--- cmd/commit.go
+++ cmd/commit.go
@@ -18,13 +18,8 @@ import (
 // commitCmd represents the commit command
 var commitCmd = &cobra.Command{
 	Use:   "commit",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Commit the gitcloak and map with git commit",
+	Long:  "Commit the gitcloak and map with git commit",
 	Run: func(cmd *cobra.Command, args []string) {
 		Info("gitcloak commit called")
 		commitMessage, err := cmd.Flags().GetString("message")
